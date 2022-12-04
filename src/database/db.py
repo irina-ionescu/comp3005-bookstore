d8e9296a05d4a1b8d62833c9841b299c5a9f29e7
irina-ionescu <irinatirian@gmail.com>
2022-12-03 19:58:55 -0500

    Delete and add book

--- src/database/db.py
+++ src/database/db.py
@@ -18,6 +18,7 @@ def getAllBooks():
   cur = conn.cursor()
   cur.execute("SELECT * FROM Book")
   rows = cur.fetchall()
+  cur.close()
   conn.close()
   return rows
 
@@ -27,7 +28,30 @@ def searchBooks(ISBN=None,author=None,title=None,genre=None):
   query = "SELECT * FROM Book WHERE ISBN LIKE %s OR author LIKE %s OR title LIKE %s OR genre LIKE %s"
   cur.execute(query, (ISBN, author, title, genre))
   rows = cur.fetchall()
+  cur.close()
   conn.close()
   return rows
 
 
+def deleteBookById(bookId):
+  conn = getConn()
+  cur = conn.cursor()
+  query = "DELETE FROM Book WHERE bookId=%s"
+  cur.execute(query, (bookId,))
+  conn.commit()
+  cur.close()
+  conn.close()
+
+
+def addBook(ISBN, author, title, genre, stock, price, percentRoyalty, noPages, pubId):
+  conn = getConn()
+  cur = conn.cursor()
+  query = "INSERT INTO Book(bookId, ISBN, author, title, genre, stock, price, percentRoyalty, noPages, pubId) "
+  query += "VALUES (DEFAULT, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+  cur.execute(query, (ISBN, author, title, genre, stock, price, percentRoyalty, noPages, pubId))
+  conn.commit()
+  cur.close()
+  conn.close()
+
+#deleteBookById(15)
+#addBook("1234-5678-9103", "Irina Ionescu", "Adventures in Postgres", "Drama", 1, 10000.01, 100, 1, 4 )
