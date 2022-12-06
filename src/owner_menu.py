306c8841cd47c3d2baa7b9ad0d7931de7fd40663
irina-ionescu <irinatirian@gmail.com>
2022-12-05 20:29:05 -0500

    Pretty menu

--- src/owner_menu.py
+++ src/owner_menu.py
@@ -2,23 +2,27 @@ import util as util
 
   
 def getOChoiceMainMenu()->int :
-    choice = util.getValidIntInput("""What would you like to do?
-                              1. List all books
-                              2. Add new books
-                              3. Remove books
-                              4. List publishers
-                              5. Add publisher
-                              6. Generate reports
-                              0. return to previous menu 
-                              Choice: """, 0,6)
+    choice = util.getValidIntInput("""
+    ---------------------------
+    What would you like to do?
+    1. List all books
+    2. Add new book
+    3. Remove book
+    4. List publishers
+    5. Add publisher
+    6. Generate reports
+    0. Exit 
+    Choice: """, 0,6)
     return choice
 
 def getOChoiceReports()->int:
-  choice = util.getValidIntInput("""What would you like to do?
-                              1. Sales vs. expenditures
-                              2. Sales by parameter (example: author, genre, ...)
-                              3. Get orders placed to publishers
-                              0. return to previous menu 
-                              Choice: """, 0,3)
+  choice = util.getValidIntInput("""
+  ---------------------------
+  What would you like to do?
+  1. Sales vs. expenditures
+  2. Sales by parameter (example: author, genre, ...)
+  3. Get orders placed to publishers
+  0. Return to previous menu 
+  Choice: """, 0,3)
   return choice
   
