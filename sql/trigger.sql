9a1bf305ee70ff7b0512e4640de1482307fceb7d
irina-ionescu <irinatirian@gmail.com>
2022-12-09 21:36:26 -0500

    trigger wip

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -1,13 +1,13 @@
-CREATE OR REPLACE FUNCTION insert_supply_order()
-	RETURNS trigger AS $insert_supply_order$
-	BEGIN
-		IF NEW.stock < 10 THEN
-			INSERT INTO supplyorder(supid, supdate, quantity, pubid, bookid)
-			VALUES (DEFAULT, CURRENT_DATE, 10, NEW.pubId, NEW.bookId);
-		END IF;
-		RETURN NEW;
-	END;
-$insert_supply_order$ LANGUAGE plpgsql
+	CREATE OR REPLACE FUNCTION insert_supply_order()
+		RETURNS trigger AS $insert_supply_order$
+		BEGIN
+			IF NEW.stock < 10 THEN
+				INSERT INTO supplyorder(supid, supdate, quantity, pubid, bookid)
+				VALUES (DEFAULT, CURRENT_DATE, 10, NEW.pubId, NEW.bookId);
+			END IF;
+			RETURN NEW;
+		END;
+	$insert_supply_order$ LANGUAGE plpgsql
 
-CREATE TRIGGER insert_supply_order AFTER UPDATE ON book
-    FOR EACH ROW EXECUTE FUNCTION insert_supply_order();
+	CREATE TRIGGER insert_supply_order AFTER UPDATE ON book
+			FOR EACH ROW EXECUTE FUNCTION insert_supply_order();
